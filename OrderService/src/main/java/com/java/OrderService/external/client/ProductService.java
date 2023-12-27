42468447545431fd6d26f962a7ed02ab3fc9db38
JackeyyLove <loidao99@gmail.com>
2023-12-27 19:54:34 +0700

    implement external circuit breakerin order service

--- OrderService/src/main/java/com/java/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/java/OrderService/external/client/ProductService.java
@@ -1,5 +1,7 @@
 package com.java.OrderService.external.client;
 
+import com.java.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("PRODUCT-SERVICE/product")
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") Long productId, @RequestParam Long quantity);
+    default void fallback(Exception e) {
+        throw new CustomException("Product service is not available", "UNAVAILABLE", 500);
+    }
 }
