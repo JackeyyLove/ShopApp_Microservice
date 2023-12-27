e4b14c758450962969bd3af40df9f6d69777fad3
JackeyyLove <loidao99@gmail.com>
2023-12-27 18:48:36 +0700

    Create circuit breaker for gateway service

--- CloudGateway/src/main/java/com/java/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/java/CloudGateway/CloudGatewayApplication.java
@@ -1,13 +1,41 @@
 package com.java.CloudGateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4jBulkheadConfigurationBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.time.Duration;
 
 @SpringBootApplication
+@Configuration
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+				.failureRateThreshold(50)
+				.waitDurationInOpenState(Duration.ofMillis(1000))
+				.slidingWindowSize(2)
+				.build();
+		TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+				.timeoutDuration(Duration.ofSeconds(4))
+				.build();
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.timeLimiterConfig(timeLimiterConfig)
+				.circuitBreakerConfig(circuitBreakerConfig)
+				.build());
+	}
 }
